500c1b2a62e1db0c25248470a233e8592a466836
Patrick J. Clemins <patrick.clemins@uvm.edu>
2024-05-29 04:21:06 +0000

    Near Final Version for DevCon24

--- pythonv2/modules.py
+++ pythonv2/modules.py
@@ -2,6 +2,9 @@ import dataretrieval.nwis as nwis
 from s3fs import S3FileSystem, S3Map
 import xarray as xr
 import matplotlib.pyplot as plt
+import seaborn as sns
+import holoviews as hv
+import plotly.express as px
 
 def acquire_streamflow_nwis_iv(site, start, end):
     df = nwis.get_record(sites=site, service='iv', start=start, end=end, parameterCD='00060')
@@ -18,12 +21,16 @@ def acquire_streamflow_nwm_retrospective(site, start, end):
 
     return ds.get('streamflow').to_pandas().rename('streamflow (m^3 s^-1)')
 
+#######################################
+
 def resample_to_daily(df):
     return df.resample('1D').mean()
 
 def cubicftsec_to_cubicmsec(series):
     new_name = series.name.split('(')[0] + '(m^3 s^-1)'
     return (series * 0.0283168).rename(new_name)
+
+#######################################
 
 def visualize_summary_statistics(df):
     print(df.describe())
@@ -38,3 +45,22 @@ def visualize_matplotlib_comparison(series1, series2, save_figure = False):
         plt.savefig('comparison.png')
     else:
         plt.show()
+
+def visualize_matplotlib(series):
+    fig, ax = plt.subplots()
+    ax.plot(series, linewidth=2.0)
+    ax.set_title(series.name)
+    plt.show()
+
+def visualize_seaborn(series):
+    sns.lineplot(series)
+
+# Use as: hv.render(visualize_holoviews(series))
+def visualize_holoviews(series):
+    return hv.Curve(series)
+
+def visualize_plotly(series):
+    fig = px.line(series, title=series.name)
+    fig.show()
+
+
